1abb1a2577f78ab6ed7c46442935a26f5d66c10e
zanjs <root@zanjs.com>
2016-04-01 17:14:41 +0800

    :cat: init

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    background: path.resolve(__dirname, 'scripts.babel', 'background.js'),
+    popup: path.resolve(__dirname, 'scripts.babel', 'popup.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'app', 'scripts.build'),
+    publicPath: '/scripts.build',
+    filename: '[name].js',
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+  resolve: {
+    root: path.resolve(__dirname, 'node_modules'),
+    extensions: ['', '.js', '.css', '.vue']
+  },
+  module: {
+    loaders: [{
+      test: /\.vue$/,
+      loader: 'vue'
+    }, {
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'url',
+      query: {
+        limit: 10000,
+        name: '[name].[ext]?[hash]'
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  watch: true,
+  externals: {
+    'jquery': 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ],
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  devtool: 'cheap-module-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = 'source-map';
+  module.exports.watch = false;
+  // http://vuejs.github.io/vue-loader/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]);
+}
